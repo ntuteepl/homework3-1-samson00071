164f1b80dc10ca0f1af4862c09e8caf7d5fb75df
samson00071 <91417049+samson00071@users.noreply.github.com>
2023-10-23 22:38:18 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char answer[5]; 
+    char guess[5]; 
+    int round = 0;
+
+    scanf("%s", answer);
+
+    while (1) {
+
+        scanf("%s", guess);
+
+
+        if (strcmp(guess, "0") == 0) {
+            break;
+        }
+
+        int A = 0, B = 0;
+
+
+        for (int i = 0; i < 4; i++) {
+            if (guess[i] == answer[i]) {
+                A++;
+            } else {
+                for (int j = 0; j < 4; j++) {
+                    if (guess[i] == answer[j]) {
+                        B++;
+                    }
+                }
+            }
+        }
+
+        printf("%dA%dB\n", A, B);
+
+        round++;
+    }
+
+    return 0;
+}
 
